f889a5efcc75381436e3c0bc4b409955a72c225f
seth <sowirodu@trinity.edu>
2023-06-27 19:31:28 +0000

    Making functions

--- pair-programming.py
+++ pair-programming.py
@@ -1,25 +1,24 @@
-import requests
+import requests as rq
 import pandas as pd
 import os
+import sqlalchemy as db
 
+def hours_needed(food_name):
+        ninja_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(food_name)
+        response = rq.get(ninja_url, headers={'X-Api-Key': os.environ.get('API_KEY')})
+        if response.status_code == rq.codes.ok:
+                print(response.text)
+        else:
+            print("Error:", response.status_code, response.text)
 
-food_name = input('Input a type of food: ')
+        food_data = response.json()
+        food_data_df = pd.DataFrame.from_dict(food_data)
+        engine = db.create_engine('sqlite:///food_data.db')
+        food_data_df.to_sql('food_table', con=engine, if_exists='replace', index=False)
+        with engine.connect() as connection:
+                query_result = connection.execute(db.text("SELECT * FROM food_table;")).fetchall()
+                food_db = pd.DataFrame(query_result)
+                print(food_db)
 
-api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(food_name)
-response = requests.get(api_url, headers={'X-Api-Key': os.environ.get('API_KEY')})
-if response.status_code == requests.codes.ok:
-    print(response.text)
-else:
-    print("Error:", response.status_code, response.text)
-
-food_data = response.json()
-food_data_df = pd.DataFrame.from_dict(food_data)
-
-row_num = len(food_data_df)
-total_calories = 0
-for i in range(row_num):
-    total_calories = total_calories + food_data_df['calories'][i]
-
-
-
-
+#tesing
+hours_needed('brisket')
