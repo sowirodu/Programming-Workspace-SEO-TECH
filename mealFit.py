2746b3804827f2c42da90221400ea4db88afc0ea
Seth Owirodu <sowirodu@trinity.edu>
2023-06-29 20:24:49 +0000

    Finished up everything

--- mealFit.py
+++ mealFit.py
@@ -3,55 +3,71 @@ import pandas as pd
 import os
 import sqlalchemy as db
 
-def retr_Food(user_Food):
-        ninja_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(user_Food)
-        response = rq.get(ninja_url, headers={'X-Api-Key': os.environ.get('API_KEY')})
-        if response.status_code == 200:
-                food_data = response.json()
-                return food_data
-        else:
-                print("Error:", response.status_code, response.text)
-                return False
 
-#how do you add weight in the url request??? ist doesn't say on the API page. right now its just the default
-def calories_per_hour(activity, weight):
-        cal_url = 'https://api.api-ninjas.com/v1/caloriesburned?activity={}'.format(activity)
-        cal_response = rq.get(cal_url, headers={'X-Api-Key': os.environ.get('API_KEY')})
-        if cal_response.status_code == 200:
-                activity_data = cal_response.json()
-                activity_df = pd.DataFrame.from_dict(activity_data)
-                cal_per_hour = activity_df['total_calories'][0]
-                return cal_per_hour
+def retr_Food(user_Food):
+    ninja_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(user_Food)
+    response = rq.get(ninja_url, headers={'X-Api-Key': os.environ.get('API_KEY')})
+    if response.status_code == 200:
+        food_data = response.json()
+        return food_data
+    else:
+        print("Error:", response.status_code, response.text)
+        return False
+
+
+def calories_per_hour(activity):
+    cal_url = 'https://api.api-ninjas.com/v1/caloriesburned?activity={}'.format(activity)
+    cal_response = rq.get(cal_url, headers={'X-Api-Key': os.environ.get('API_KEY')})
+    if cal_response.status_code == 200:
+        activity_data = cal_response.json()
+        if activity_data:
+            activity_df = pd.DataFrame.from_dict(activity_data)
+            cal_per_hour = activity_df['total_calories'][0]
+            return cal_per_hour
         else:
-                print("Error:", response.status_code, response.text())
-                return False
+            return False
+    else:
+        return False
+
 
 def to_FoodDatabase(food_data):
-        food_data_df = pd.DataFrame.from_dict(food_data)
-        engine = db.create_engine('sqlite:///food_data.db')
-        food_data_df.to_sql('food_table', con=engine, if_exists='append', index=False)
-        with engine.connect() as connection:
-                query_result = connection.execute(db.text("SELECT * FROM food_table;")).fetchall()
-                food_db = pd.DataFrame(query_result)
-        
-        return food_db
+    food_data_df = pd.DataFrame.from_dict(food_data)
+    engine = db.create_engine('sqlite:///food_data.db')
+    food_data_df.to_sql('food_table', con=engine, if_exists='append', index=False)
+    with engine.connect() as connection:
+        query_result = connection.execute(db.text("SELECT * FROM food_table;")).fetchall()
+        food_db = pd.DataFrame(query_result)
+    return food_db
+
+
+def query_FoodDatabase():
+    engine = db.create_engine('sqlite:///food_data.db')
+    with engine.connect() as connection:
+        query_result = connection.execute(db.text("SELECT * FROM food_table ORDER BY calories DESC;")).fetchall()
+        food_db = pd.DataFrame(query_result)
+    return food_db
+
 
 def main():
-        user_Food = input("Enter a food name: ")
-        user_activity = input("Enter desired activity: ")
-        user_weight = input("Enter weight: ")
-        cals_per_hour = calories_per_hour(user_activity, user_weight)
+    while True:
+        user_input = input("Enter a food name (type 'log' to exit): ")
 
+        if user_input.lower() == "log":
+            print(query_FoodDatabase())
+            break
+
+        user_Food = user_input
+        user_activity = input("Enter desired activity: ")
+        cals_per_hour = calories_per_hour(user_activity)
         food_data = retr_Food(user_Food)
-        if (food_data and cals_per_hour):
-                food_db = to_FoodDatabase(food_data)
-                print(food_db)
-                cal_col = food_db['calories']
-                total_cals = 0
-                for x in cal_col:
-                    total_cals = total_cals + x 
-                hours = int(total_cals/cals_per_hour)
-                print("Total hours needed to burn calories is:" , hours)
+
+        if food_data and cals_per_hour:
+            food_db = to_FoodDatabase(food_data)
+            cal_col = food_db['calories']
+            total_cals = cal_col.sum()
+            hours = int(total_cals / cals_per_hour)
+            print("Total hours needed to burn calories:", hours)
+
 
 if __name__ == "__main__":
     main()
--- unitTests.py
+++ unitTests.py
@@ -2,10 +2,21 @@ import unittest
 from mealFit import *
 
 class TestFoodProject(unittest.TestCase):
-    def test_retr_Food_valid_response(self):
-        food_data = retr_Food('brisket')
-        self.assertEqual(food_data, {'food_name': 'brisket', 'calories': 289.3})
 
     def test_retr_Food_invalid_response(self):
         food_data = retr_Food('Test Food')
         self.assertFalse(food_data)
+
+    def test_retr_Food_valid_response(self):
+            food_data = retr_Food('brisket')
+            self.assertTrue(food_data )
+
+    def test_calories_per_hour_invalid_response(self):
+        cal_per_hour = calories_per_hour('unknown_activity')
+        self.assertFalse(cal_per_hour)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
